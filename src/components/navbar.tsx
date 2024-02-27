5c376730c2f2eb55b1d8cf54fea1698d41816e09
elvinega <proega3@gmail.com>
2024-02-27 21:52:28 +0300

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="flex justify-center items-center self-stretch px-16 py-3.5 w-full bg-white border-solid border-b-[0.5px] border-b-slate-200 ">
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/877760cefe3fa3b3f02304aa5b685b93a8d3a0f35c6a0db3468cd6c900e27eb6?"
+        className="max-w-full aspect-[3.33] w-[108px]"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
