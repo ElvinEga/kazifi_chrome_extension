fc6ee0c7dc202bdbae5a6092b62acd9afd0caf2d
Elvin Ega <proega3@gmail.com>
2024-03-24 14:33:49 +0300

    remove landing components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import ScanJobPage from "./pages/ScanJob";
 import ResultsPage from "./pages/Results";
-import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/Home";
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
     <>
       <Routes>
         {/* public routes */}
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/extension" element={<HomePage />} />
         <Route path="/scan" element={<ScanJobPage />} />
         <Route path="/results" element={<ResultsPage />} />
